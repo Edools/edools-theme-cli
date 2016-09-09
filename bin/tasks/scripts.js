12e13943fdb5e08317f4f87977f2083715c72f97
mayke5421 <mayke_sargento@hotmail.com>
2016-09-09 17:13:45 -0300

    add support to ES6

--- bin/tasks/scripts.js
+++ bin/tasks/scripts.js
@@ -2,6 +2,7 @@
 
 let gulp = require('gulp');
 let $ = require('gulp-load-plugins')();
+let es2015 = require('babel-preset-es2015');
 let config = require('../config');
 let wiredep = require('wiredep');
 
@@ -24,8 +25,11 @@ gulp.task('js:vendors', () => {
 gulp.task('js', () => {
   return gulp.src(config.files.js)
     .pipe($.sourcemaps.init())
-    .pipe($.uglify().on('error', config.errorHandler('uglify')))
     .pipe($.concat(config.build.js))
+    .pipe($.babel({
+      presets: [es2015]
+    }))
+    .pipe($.uglify().on('error', config.errorHandler('uglify')))
     .pipe($.sourcemaps.write('.'))
     .pipe(gulp.dest(config.paths.dist + config.paths.assets))
     .pipe(gulp.dest(config.paths.compiled));
